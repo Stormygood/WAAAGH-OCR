80390bfd75ab6672f5bcdb8550cd54f51b963b31
Stormy <122821158+Stormygood@users.noreply.github.com>
2026-06-04 01:19:07 +0300

    this work focuses on the dirty of finding the correct html elements and putting them into an array

--- test_run_1.py
+++ test_run_1.py
@@ -1,9 +1,14 @@
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 
-with open('.\\test_images\\Datasheets_all.html', 'r', encoding='utf-8') as datasheets:
-    soup = BeautifulSoup(datasheets, 'html.parser');
+# with open('.\\test_images\\Datasheets_all.html', 'r', encoding='utf-8') as datasheets:
+#     soup = BeautifulSoup(datasheets, 'html.parser');
 
-datasheetsArr = [];
+# datasheetsArr = [];
 
-# for anchor in 
+# # for anchor in 
 
+from bs4 import BeautifulSoup as BS
+
+# soup = BS(html)
+# elem = soup.findAll('a', {'title': 'title here'})
+# elem[0].text
